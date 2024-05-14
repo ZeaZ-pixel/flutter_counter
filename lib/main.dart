3385e66ab000608cc18374de6ed894bcd81a9a1d
Nariman <onlanovnar@gmail.com>
2024-05-14 14:59:54 +0500

    added list

--- lib/main.dart
+++ lib/main.dart
@@ -31,138 +31,33 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
-        body: _buildBody(),
+        body: BodyListView(),
       ),
     );
   }
 }
 
-Widget _buildBody() {
-  return SingleChildScrollView(
-    child: Column(
-      children: [
-        _headerImage(),
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                _weatherDescription(),
-                const Divider(),
-                _temperature(),
-                const Divider(),
-                _temperatureForecast(),
-                const Divider(),
-                _footerRating(),
-              ],
-            ),
-          ),
-        )
-      ],
-    ),
-  );
+class BodyListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView();
+  }
 }
 
-Image _headerImage() {
-  return const Image(
-    image: NetworkImage(
-        'https://img.freepik.com/premium-vector/geometric-background-bg-shape-background-square-background-polygonal-background-abstract-bg_634868-12.jpg'),
-    fit: BoxFit.cover,
-  );
-}
-
-Column _weatherDescription() {
-  return const Column(
-    children: [
-      Text(
-        'Tuesday - May 22',
-        style: TextStyle(
-          fontSize: 23,
-          fontWeight: FontWeight.bold,
-        ),
+Widget _myListView() {
+  return ListView(
+    scrollDirection: Axis.horizontal,
+    itemExtent: 300,
+    children: const [
+      ListTile(
+        title: Text("hello"),
+        subtitle: Text('subtitle'),
+        leading: Icon(Icons.wb_sunny),
+        trailing: Icon(Icons.keyboard_arrow_right),
       ),
-      Divider(),
-      Text(
-        'Text TextText',
-        style: TextStyle(
-            fontSize: 23, fontWeight: FontWeight.bold, color: Colors.black54),
-      ),
-    ],
-  );
-}
-
-Row _temperature() {
-  return const Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Icon(Icons.wb_sunny, color: Colors.yellow),
-        ],
-      ),
-      SizedBox(
-        width: 16.0,
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [Text("15 Clear")],
-          ),
-          Row(
-            children: [Text("dedede dedede dede")],
-          )
-        ],
-      )
-    ],
-  );
-}
-
-Wrap _temperatureForecast() {
-  return Wrap(
-    spacing: 10.0,
-    children: List<Widget>.generate(8, (int index) {
-      return Chip(
-        label: Text('${index + 20}C', style: const TextStyle(fontSize: 15)),
-        avatar: Icon(Icons.wb_cloudy, color: Colors.blue.shade300),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4),
-            side: const BorderSide(color: Colors.grey)),
-        backgroundColor: Colors.white,
-      );
-    }),
-  );
-}
-
-Row _footerRating() {
-  var stars = Row(
-    children: [
-      Icon(
-        Icons.star,
-        size: 15,
-        color: Colors.yellow[600],
-      ),
-      Icon(
-        Icons.star,
-        size: 15,
-        color: Colors.yellow[600],
-      ),
-      Icon(
-        Icons.star,
-        size: 15,
-        color: Colors.yellow[600],
-      )
-    ],
-  );
-  return Row(
-    children: [
-      const Text(
-        'Info woth open',
-        style: TextStyle(fontSize: 15.0),
-      ),
-      stars,
+      ListTile(title: Text("hello")),
+      ListTile(title: Text("hello")),
+      ListTile(title: Text("hello")),
     ],
   );
 }
