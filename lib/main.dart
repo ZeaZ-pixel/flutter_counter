62c4bf1d03a7d4dfa350457e808388ce32d1f7b8
Nariman <onlanovnar@gmail.com>
2024-05-14 14:51:31 +0500

    created simple project

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,168 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MyApp());
+  return runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          title: const Text(
-            "Counter",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+          title: const Text("Weather", style: TextStyle(color: Colors.black87)),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
           ),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Tap "-" to decrement',
-                style: TextStyle(color: Colors.white),
-              ),
-              SizedBox(height: 10),
-              CounterWidget(),
-              SizedBox(height: 10),
-              Text(
-                'Tap "+" to increment',
-                style: TextStyle(color: Colors.white),
-              )
-            ],
+          iconTheme: const IconThemeData(
+            color: Colors.black54,
           ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings),
+            )
+          ],
         ),
-        backgroundColor: const Color.fromARGB(255, 134, 152, 253),
+        body: _buildBody(),
       ),
     );
   }
 }
 
-class CounterWidget extends StatefulWidget {
-  const CounterWidget({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _CounterWidgetState();
-  }
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        _headerImage(),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _weatherDescription(),
+                const Divider(),
+                _temperature(),
+                const Divider(),
+                _temperatureForecast(),
+                const Divider(),
+                _footerRating(),
+              ],
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }
 
-class _CounterWidgetState extends State<CounterWidget> {
-  int _counter = 44;
+Image _headerImage() {
+  return const Image(
+    image: NetworkImage(
+        'https://img.freepik.com/premium-vector/geometric-background-bg-shape-background-square-background-polygonal-background-abstract-bg_634868-12.jpg'),
+    fit: BoxFit.cover,
+  );
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return IntrinsicWidth(
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  _counter -= 1;
-                });
-              },
-              child: const Icon(Icons.remove),
-            ),
-            const SizedBox(width: 10),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 24),
-            ),
-            const SizedBox(width: 10),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  _counter += 1;
-                });
-              },
-              child: const Icon(Icons.add),
-            ),
-          ],
+Column _weatherDescription() {
+  return const Column(
+    children: [
+      Text(
+        'Tuesday - May 22',
+        style: TextStyle(
+          fontSize: 23,
+          fontWeight: FontWeight.bold,
         ),
       ),
-    );
-  }
+      Divider(),
+      Text(
+        'Text TextText',
+        style: TextStyle(
+            fontSize: 23, fontWeight: FontWeight.bold, color: Colors.black54),
+      ),
+    ],
+  );
+}
+
+Row _temperature() {
+  return const Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(Icons.wb_sunny, color: Colors.yellow),
+        ],
+      ),
+      SizedBox(
+        width: 16.0,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [Text("15 Clear")],
+          ),
+          Row(
+            children: [Text("dedede dedede dede")],
+          )
+        ],
+      )
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10.0,
+    children: List<Widget>.generate(8, (int index) {
+      return Chip(
+        label: Text('${index + 20}C', style: const TextStyle(fontSize: 15)),
+        avatar: Icon(Icons.wb_cloudy, color: Colors.blue.shade300),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+            side: const BorderSide(color: Colors.grey)),
+        backgroundColor: Colors.white,
+      );
+    }),
+  );
+}
+
+Row _footerRating() {
+  var stars = Row(
+    children: [
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.yellow[600],
+      )
+    ],
+  );
+  return Row(
+    children: [
+      const Text(
+        'Info woth open',
+        style: TextStyle(fontSize: 15.0),
+      ),
+      stars,
+    ],
+  );
 }
